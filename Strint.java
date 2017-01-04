1a7111ec12a5b58364cb7119e10c13ed0fcb1cc5
vigneshsara28 <srvicky6628@gmail.com>
2017-01-04 18:16:50 +0530

    String to integer

---
+++ Strint.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Strint {
+		public static void main(String[] args) {	
+			 Scanner sc=new Scanner(System.in);
+			 String str=sc.nextLine();
+			 int d=Integer.parseInt(str);
+			 System.out.println("Integer is: " +d);
+			}
+	}
