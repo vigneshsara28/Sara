9e8b48955557d14f934487572adf12d22daa4034
vigneshsara28 <srvicky6628@gmail.com>
2017-01-04 13:28:16 +0530

    Only one  number

---
+++ Unique1.java
@@ -0,0 +1,33 @@
+package kumar;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Unique1 {
+
+	public static void main(String[] args) {	
+		 Scanner sc=new Scanner(System.in);
+		 int n=sc.nextInt();
+		 int a[]=new int[n];
+		 int c=0;
+		 for(int i=0;i<n;i++)
+		 {
+			 a[i]=sc.nextInt();
+		 }
+		 int temp=0;
+		 Arrays.sort(a);
+		 for(int i=0;i<=n/2;i=i+2)
+		 {
+			if(a[i]==a[i+1])
+				c++;
+			if(a[i]!=a[i+1]) 
+				temp=a[i];
+		 }
+		 if(c==n/2)
+		 System.out.println(a[n-1]);
+		 else if(c==0)
+			 System.out.println(a[0]);
+		 else
+			 System.out.println(temp);
+		 }
+}
