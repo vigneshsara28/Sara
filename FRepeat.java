88934569744817dd305153e5c5e69a8ec26d20e1
vigneshsara28 <srvicky6628@gmail.com>
2017-01-04 13:22:09 +0530

    Add files via upload

---
+++ FRepeat.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FRepeat {
+
+	public static void main(String[] args) {	
+		 Scanner sc=new Scanner(System.in);
+		 int n=sc.nextInt();
+		 int a[]=new int[n];
+		 int b[]=new int[n];
+		 int c=0;
+		 for(int i=0;i<n;i++)
+		 {
+			 a[i]=sc.nextInt();
+		 }
+		 Arrays.sort(a);
+		 for(int i=0;i<n-1;i++)
+		 {
+		 if(a[i]==a[i+1]){
+			 b[c]=a[i];
+		 c++;
+		 } 
+		 }
+		 if(c==0)
+		 {
+			 System.out.println("No repeated numbers");
+		 }
+			 System.out.println(b[0]);
+		 }
+}
